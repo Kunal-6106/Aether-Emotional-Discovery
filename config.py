f4d70ca59cda31b8ab65d975c6c05e10c9d8197a
Kunal-6106 <6106kunalpandey@gmail.com>
2026-05-08 22:13:16 +0530

    Removed sensitive config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '8779408291'
+    MYSQL_PASSWORD = 'your_password'
     MYSQL_DATABASE = 'aether_db'
     SECRET_KEY = 'aether_luxury_key_2026'
